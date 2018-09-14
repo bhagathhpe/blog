6ae356aea2e2a1782acbca719c62d75b836d28d6
Bhagath Reddy <katta-bhagath.reddy@hpe.com>
2018-09-14 03:55:37 -0700

    blog creation updation deletion crud operation finished

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,47 @@
+class ArticlesController < ApplicationController
+  before_action :find_article, except: [:index, :new, :create]	
+  def index
+  	@articles = Article.all
+  end
+
+  def new
+  	@article = Article.new
+  end
+
+  def create
+  	@article = Article.new(article_params)
+    if @article.save
+      redirect_to @article
+    else
+	  render :new
+    end
+  end
+
+  def edit
+  end
+
+  def update
+  	if @article.update(article_params)
+      redirect_to @article
+    else
+	  render :edit
+	end
+  end
+
+  def show
+  end
+
+  def destroy
+  	@article.destroy
+  	redirect_to articles_path
+  end
+
+  private
+  
+  def find_article
+  	@article = Article.find(params[:id])
+  end
+  def article_params
+  	params.require(:article).permit!
+  end
+end
